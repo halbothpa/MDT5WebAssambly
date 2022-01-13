15ccca5cd6988fea6520b191db79338e5ce1570a
Patrick Halboth <halbothpa61667@th-nuernberg.de>
2022-01-13 14:53:22 +0200

    gg

--- Program.cs
+++ Program.cs
@@ -1,16 +1,19 @@
 using System; 
+
 using System.IO; 
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using RestSharp;
-using RestSharp.Authenticators;
+using Newtonsoft.Json;
+
 namespace MDT5WebAssembly
 {
     public class Program
