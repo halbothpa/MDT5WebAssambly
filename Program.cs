b3add6e677688dd2f7a7922f40b5c2792b2cb964
Patrick Halboth <halbothpa61667@th-nuernberg.de>
2022-01-15 22:08:27 +0200

    reanme all

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace MDT5WebAssambly
+namespace MDT5WebAssembly
 {
     public class Program
     {
